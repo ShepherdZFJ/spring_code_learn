319ab6f6dddbdcd7e037d8a74dc081c8b572644f
shepherd <zhengfj@isyscore.com>
2021-01-27 11:28:07 +0800

    add: 添加注释

--- framework_core/src/main/java/com/shepherd/proxy/jdk/JDKProxyMain.java
+++ framework_core/src/main/java/com/shepherd/proxy/jdk/JDKProxyMain.java
@@ -12,6 +12,14 @@ import java.lang.reflect.Proxy;
 public class JDKProxyMain {
 
     public static void main(String[] args) {
+        /**
+         * 要求：被代理类至少实现一个接口
+         * newProxyInstance(ClassLoader loader, Class<?>[] interfaces, InvocationHandler h)
+         * 参数含义：
+         *         ClassLoader：和被代理对象使用相同的类加载器。
+         *         Interfaces：和被代理对象具有相同的行为。实现相同的接口。
+         *         InvocationHandler：如何代理，使用反射执行接口原方法，同时可扩展增强方法，如下面的示例JDKProxyTestInvocationHandler。
+         */
         JDKProxyTestInterface target = new JDKProxyTestInterfaceImpl();
         // 根据目标对象创建代理对象
         JDKProxyTestInterface proxy = (JDKProxyTestInterface) Proxy
@@ -36,6 +44,7 @@ public class JDKProxyMain {
         }
     }
 
+    //InvocationHandler实现类，使用反射增强方法
     static class JDKProxyTestInvocationHandler implements InvocationHandler {
         private Object target;
         public JDKProxyTestInvocationHandler(Object target) {
